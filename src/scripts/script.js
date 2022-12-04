acddfcfff1be5673d15cdf31de3e5ee545fa3acf
Mak805 <laurenz.heyberg@students.hs-mainz.de>
2022-12-04 18:49:31 +0100

    map fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,8 @@
-var map = L.map('map', {
+var map = L.map('map',{
     center: [49.988799, 8.227393],
     zoom: 14
 });
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
