ad7296e0d48f1967137410e8d201b8afbf6ea531
Marius Klein <marius.klein@students.hs-mainz.de>
2022-12-06 21:42:47 +0100

    Started development on routing feature
    Signed-off-by: InSuchtKatze <44026380+InSuchtKatze@users.noreply.github.com>

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map',{
+var map = L.map('map', {
     center: [49.988799, 8.227393],
     zoom: 14
 });
@@ -6,27 +6,50 @@ var map = L.map('map',{
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
-var hidemenu=true;
-var menu=0;
+var hidemenu = true;
+var menu = 0;
 myFunction();
 
+var switcher = false;
+var lat1, lon1;
 
-function myFunction(){
+function createRoute(fromLat, fromLon, toLat, toLon) {
+    L.Routing.control({
+        waypoints: [
+            L.latLng(fromLat, fromLon),
+            L.latLng(toLat, toLon)
+        ],
+        serviceUrl: "https://router.project-osrm.org/route/v1",
+        routeWhileDragging: false
+    }).addTo(map);
+}
+//Utility only
+map.on('click', function(e) {
+    if(switcher === false){
+        lat1 = e.latlng.lat;
+        lon1 = e.latlng.lng;
+        switcher = true;
+    } else {
+        createRoute(lat1, lon1, e.latlng.lat, e.latlng.lng);
+        switcher = false;
+    }
+});
+
+function myFunction() {
     //alert("ey")
     var divbuttons = document.getElementById("buttons");
     var divcheckboxPoi = document.getElementById("checkboxPoi");
     var divslidecontainer = document.getElementById("slidecontainer");
     var divbuttons_walking_bike_car = document.getElementById("buttons_walking-bike-car");
-    if (hidemenu == true){
+    if (hidemenu == true) {
 
 
         document.getElementById("menu").style.background = "transparent";
         divbuttons.style.display = "none";
-        menu=0;
+        menu = 0;
         showmenu();
         hidemenu = false;
-    }
-    else{
+    } else {
         document.getElementById("menu").style.background = "grey";
         divbuttons.style.display = "block";
         showmenu();
@@ -34,34 +57,34 @@ function myFunction(){
     }
 }
 
-function showmenu(){
+function showmenu() {
     document.getElementById("buttons");
     var divcheckboxPoi = document.getElementById("checkboxPoi");
     var divslidecontainer = document.getElementById("slidecontainer");
     var divbuttons_walking_bike_car = document.getElementById("buttons_walking-bike-car");
-    if (menu ==0){
+    if (menu == 0) {
         divcheckboxPoi.style.display = "none";
         divslidecontainer.style.display = "none";
         divbuttons_walking_bike_car.style.display = "none";
     }
-    if (menu == 1){
+    if (menu == 1) {
         divcheckboxPoi.style.display = "block";
         divslidecontainer.style.display = "none";
         divbuttons_walking_bike_car.style.display = "none";
     }
-    if (menu ==2){
+    if (menu == 2) {
         divcheckboxPoi.style.display = "none";
         divslidecontainer.style.display = "none";
         divbuttons_walking_bike_car.style.display = "block";
     }
-    if (menu ==3){
+    if (menu == 3) {
         divcheckboxPoi.style.display = "none";
         divslidecontainer.style.display = "block";
         divbuttons_walking_bike_car.style.display = "none";
     }
 }
 
-function selectbutton(value){
-    menu=value;
+function selectbutton(value) {
+    menu = value;
     showmenu();
 }
