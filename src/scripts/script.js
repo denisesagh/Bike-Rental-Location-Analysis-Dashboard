7cc19f609ea0255e9f0cf67ae3eb37df62c2fad9
denis <arvin.esagholian@stundets.hs-mainz.de>
2022-12-09 20:39:21 +0100

    login window user 2.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,4 @@
+
 var osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
     osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     osm = L.tileLayer(osmUrl, {
@@ -21,7 +22,7 @@ function onMapClick(e) {
         .bindPopup(e.latlng.toString()).openPopup();
 
     // Update marker on changing it's position
-    marker.on("dragend", function (ev) {
+    marker.on("dragend", function(ev) {
 
         var chagedPos = ev.target.getLatLng();
         this.bindPopup(chagedPos.toString()).openPopup();
@@ -29,108 +30,68 @@ function onMapClick(e) {
     });
 }
 
-var hidemenu = true;
-var menu = 0;
+var hidemenu=true;
+var menu=0;
 myFunction();
 
 
-function myFunction() {
+function myFunction(){
     //alert("ey")
     var divbuttons = document.getElementById("buttons");
     var divcheckboxPoi = document.getElementById("checkboxPoi");
     var divslidecontainer = document.getElementById("slidecontainer");
     var divbuttons_walking_bike_car = document.getElementById("buttons_walking-bike-car");
+    if (hidemenu == true){
 
-    if (hidemenu === true) {
+
         document.getElementById("menu").style.background = "transparent";
         divbuttons.style.display = "none";
-        menu = 0;
-        showmenu(0);
+        menu=0;
+        showmenu();
         hidemenu = false;
-    } else {
+    }
+    else{
         document.getElementById("menu").style.background = "grey";
         divbuttons.style.display = "block";
-        //menu wird nie gesetzt?? immer nur übergeben durch html -> direkt reine übergebungsvariable machen
-        showmenu(menu);
+        showmenu();
         hidemenu = true;
-        radiusbuttons(0);
-        selectbutton(0);
     }
 }
 
-function showmenu(menu) {
+function showmenu(){
     document.getElementById("buttons");
     var divcheckboxPoi = document.getElementById("checkboxPoi");
     var divslidecontainer = document.getElementById("slidecontainer");
     var divbuttons_walking_bike_car = document.getElementById("buttons_walking-bike-car");
-
-    divcheckboxPoi.style.display = "none";
-    divslidecontainer.style.display = "none";
-    divbuttons_walking_bike_car.style.display = "none";
-
-    if (menu === 1) {
-        divcheckboxPoi.style.display = "block";
+    if (menu ==0){
+        divcheckboxPoi.style.display = "none";
+        divslidecontainer.style.display = "none";
+        divbuttons_walking_bike_car.style.display = "none";
     }
-    if (menu === 2) {
+    if (menu == 1){
+        divcheckboxPoi.style.display = "block";
+        divslidecontainer.style.display = "none";
+        divbuttons_walking_bike_car.style.display = "none";
+    }
+    if (menu ==2){
+        divcheckboxPoi.style.display = "none";
+        divslidecontainer.style.display = "none";
         divbuttons_walking_bike_car.style.display = "block";
     }
-    if (menu === 3) {
+    if (menu ==3){
+        divcheckboxPoi.style.display = "none";
         divslidecontainer.style.display = "block";
+        divbuttons_walking_bike_car.style.display = "none";
     }
 }
 
-function showUserLogin() {
+function showUserLogin(){
+
     document.getElementById("register").style.display = "none";
+
 }
 
-function selectbutton(value) {
-    menu = value;
-    showmenu(value);
-    buttonpressed(value);
-}
-
-function buttonpressed(menu) {
-    var buttonWaypoint = document.getElementById("buttonWaypoint");
-    var buttonRoute = document.getElementById("buttonRoute");
-    var buttonRadius = document.getElementById("buttonRadius");
-
-    buttonWaypoint.classList.remove("buttonselected");
-    buttonRoute.classList.remove("buttonselected");
-    buttonRadius.classList.remove("buttonselected");
-
-    if (menu === 1) {
-        buttonWaypoint.classList.add("buttonselected");
-    }
-    if (menu === 2) {
-        buttonRoute.classList.add("buttonselected");
-    }
-    if (menu === 3) {
-        buttonRadius.classList.add("buttonselected");
-    }
-}
-
-//TODO: get rid of this or add relevant functionality
-function radiusbuttons(value) {
-    buttonpressedRadius(value);
-}
-
-function buttonpressedRadius(radiusmenu) {
-    var buttonWalking = document.getElementById("buttonWalking");
-    var buttonBike = document.getElementById("buttonBike");
-    var buttonCar = document.getElementById("buttonCar");
-
-    buttonWalking.classList.remove("buttonselected");
-    buttonBike.classList.remove("buttonselected");
-    buttonCar.classList.remove("buttonselected");
-
-    if (radiusmenu === 1) {
-        buttonWalking.classList.add("buttonselected");
-    }
-    if (radiusmenu === 2) {
-        buttonBike.classList.add("buttonselected");
-    }
-    if (radiusmenu === 3) {
-        buttonCar.classList.add("buttonselected");
-    }
-
+function selectbutton(value){
+    menu=value;
+    showmenu();
 }
