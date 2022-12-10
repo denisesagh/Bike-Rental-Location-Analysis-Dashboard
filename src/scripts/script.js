7b9e7dbd8bf8e9b5bcb1d4405c7ccb42830d4aaa
InSuchtKatze <44026380+InSuchtKatze@users.noreply.github.com>
2022-12-10 11:13:51 +0100

    cleaned Denis code -> refactored functions to new JS file

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -85,36 +85,6 @@ function showmenu(){
     }
 }
 
-document.querySelector("#user_login_logout").style.display = "none";
-function isLogged(){
-    //LOGIN CODE
-    return true;
-}
-
-function ShowUserLoggedStatus(){
-    if (isLogged()){
-        console.log("User is logged");
-        document.querySelector("#user_login_logout").style.display = "block";
-        let divLoginInput = document.querySelector("body > div.user_login_window > div > label");
-        let divLoginInputSubmitter = document.querySelector("#user_login_submit")
-        let divLoginWindow = document.querySelector("body > div.user_login_window")
-        divLoginWindow.setAttribute("style", "width: 90px");
-        divLoginInput.style.display = "none";
-        divLoginInputSubmitter.style.display = "none";
-
-    }
-
-}
-
-function displayLoggedUser(){
-    alert("Eingelogged als "); //ADD USERNAME
-}
-
-function refreshPage(){
-    window.location.reload();
-}
-
-
 function selectbutton(value){
     menu=value;
     showmenu();
