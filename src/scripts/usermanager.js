2fcc0d6cc3fbdadb514bf21a3b2812c17fc1c16e
denis <arvin.esagholian@stundets.hs-mainz.de>
2022-12-13 23:18:53 +0100

    User login

--- src/scripts/usermanager.js
+++ src/scripts/usermanager.js
@@ -1,12 +1,59 @@
+var username;
+var loginStatus;
 
-function isLogged(){
-    //LOGIN CODE
-    return true;
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
 }
 
-function ShowUserLoggedStatus(){
-    if (isLogged()){
-        console.log("User is logged");
+async function isLogged(){
+    //LOGIN CODE
+    username = document.querySelector("body > div.user_login_window > div > label > input:nth-child(1)").value;
+    let password = document.querySelector("body > div.user_login_window > div > label > input:nth-child(2)").value;
+    let hashedPassword = await sha256(password);
+    console.log(username);
+    //alert (hashedPassword);
+    //PHP-Datei muss in htdocs liegen
+    try{
+
+        $.ajax({
+            url:"../scripts/userlogin.php",    //the page containing php script
+            type: "post",    //request type,
+            dataType: 'json',
+            data: {registration: "success", name: username, password: hashedPassword},
+            success:function(result){
+                alert("Erfolgreich eingeloggt als " + username);
+                loginStatus = "logged";
+                let user_id = result.id;
+                ShowUserLoggedStatus(user_id, loginStatus);
+
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(xhr.status);
+                console.log(thrownError);
+                alert("Username oder Passwort falsch");
+                refreshPage();
+            }
+        });
+
+    }catch (error){
+        console.log("AjaxSendErrorInisLoggedFunction" + error);
+    }
+}
+
+function ShowUserLoggedStatus(user_id, loginStatus){
+    if (loginStatus === "logged"){
+        console.log("User with id " + user_id + " is logged"); //USER ID!!!!!!!!!
         document.querySelector("#user_login_logout").style.display = "block";
         let divLoginInput = document.querySelector("body > div.user_login_window > div > label");
         let divLoginInputSubmitter = document.querySelector("#user_login_submit")
@@ -18,7 +65,11 @@ function ShowUserLoggedStatus(){
 }
 
 function displayLoggedUser(){
-    alert("Eingelogged als "); //ADD USERNAME
+    if (loginStatus === "logged"){
+    alert ("Eingeloggt als " + username);
+    }else{
+        alert("Du bist nicht eingeloggt");
+    }
 }
 
 function refreshPage(){
