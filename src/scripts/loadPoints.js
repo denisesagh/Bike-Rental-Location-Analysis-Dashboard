b65aa9a06e9ba7860d01371790a3ac6de6eb87a1
Mak805 <laurenz.heyberg@students.hs-mainz.de>
2022-12-19 13:36:05 +0100

    Datenbankabfrage verbessert In der HTML Datei die Scripts ans ende verschoben um Probleme zu verhindern

--- src/scripts/loadPoints.js
+++ src/scripts/loadPoints.js
@@ -1,36 +1,53 @@
 map.on('moveend', function (){
+    //placeMarkersInBounds();
+    var Box = map.getBounds();
+    //console.log(Box.toBBoxString());
+    let str = Box.toBBoxString();
+    let result = str.split(/[,]/);
+    let longStart = result[1];
+    let latStart = result[0];
+    let longEnd = result[3];
+    let latEnd = result[2];
+
+    console.log(longStart, latStart, longEnd, latEnd);
+    ajaxloadData(latStart, latEnd, longStart, longEnd);
+});
+
+
+
+function ajaxloadData(latStart, latEnd, longStart, longEnd){
     try{
-        let ajaxLoadMHSError;
         $.ajax({
-            url: ('../php/POI-Dash-GetDataPOI.php'),
-            data: {},
             type: 'GET',
-            timeout: 1000,
+            url: ('../php/POI-Dash-GetData.php'),
             dataType: 'json',
+            data: {lat1: latStart, lat2: latEnd, long1: longStart, long2: longEnd},
             error: ajaxLoadMHSError,
             success: placeMarkersInBounds
         })
     }catch (error){
         console.log("Failed to load POIS!");
     }
-});
+}
+
+function ajaxLoadMHSError(){
+    console.log("didnt work");
+}
 
 
 var markerLayer = null;
 function placeMarkersInBounds(myData) {
-
     let markerCounter = 0;
 
     var greenIcon = new L.Icon({
         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
     });
 
-    var mapBounds = map.getBounds();
+    var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        iconSize: [25, 41],
+    });
 
 
     if(markerLayer == null){
@@ -41,18 +58,33 @@ function placeMarkersInBounds(myData) {
 
     myData.forEach(element => {
 
-        markerCounter += 1;
+        if(element.Type === "POI"){
+            marker = new L.marker([element.long, element.lat], {
+                icon: redIcon,
+            })
+                .bindPopup("<b>Name: </b>" + element.POI_Name + "<br><b>Kategorie: </b>" + element.kategorie);
+        }else{
+            marker = new L.marker([element.long, element.lat], {
+                icon: greenIcon,
+            })
+                .bindPopup("<b>Name: </b>" + element.POI_Name + "<br><b>Kategorie: </b>" + element.kategorie);
+        }
 
-        marker = new L.marker([element.long, element.lat], {
-            icon: greenIcon,
-        })
-            .bindPopup("<b>Name: </b>" + element.POI_Name + "<br><b>Kategorie: </b>" + element.kategorie);
 
-        var shouldBeVisible = mapBounds.contains(marker.getLatLng());
-
-        if (shouldBeVisible && markerCounter <= 300) {
+        marker.addTo(markerLayer);
+        /*
+        if(markerCounter <= 300){
             marker.addTo(markerLayer);
         }
+        */
+
+        //markerCounter += 1;
     });
-    map.addLayer(markerLayer);
+
+    try {
+        map.addLayer(markerLayer);
+    }catch (error){
+        console.log("MarkerLayer was empty!");
+    }
+
 }
